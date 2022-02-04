a521fda336d55b786bd0cc192bd0e2a2db5dff93
Amirul Sekh <sekhemarul123@gmail.com>
2022-02-04 23:16:00 +0530

    Currently playing added

---
+++ src/components/CurrentlyPlaying.js
@@ -0,0 +1,88 @@
+import React, { useEffect, useState } from 'react';
+import api from '../config/api.json';
+import Cookies from 'js-cookie';
+import styles from '../styles/currentlyPlaying.module.css';
+
+function CurrentlyPlaying()
+{
+
+    const [currentSong, setCurrentSong] = useState({});
+    const [isLoading, setIsLoading] = useState(true);
+    
+  
+   
+  
+    const getCurrentSong = () => {
+      setIsLoading(true);
+      const url = api.BASE_URL;
+      const headers = {
+        'Authorization': 'Bearer ' + Cookies.get("spotToken"),
+      };
+      fetch(url + '/me/player/currently-playing', {
+        headers: headers
+      })
+        .then(res => res.json())
+        .then(data => {
+          console.log(data);
+          setCurrentSong(data);
+          setIsLoading(false);
+        });
+    }
+  
+    useEffect(() => {
+      getCurrentSong();
+     
+    }, []);
+  
+   
+    if(isLoading){
+      return(
+        <div className={styles.notplaying}>
+           <h1  style={{
+          textAlign: "left",
+          margin: "60px",
+         
+        }}>Currently Playing</h1>
+            <h2>NO SONG IS PLAYING </h2> 
+        </div>
+      )
+    };
+
+    const singer = currentSong.item.artists[0].name;
+    const currentimg = currentSong.item.album.images[1].url;
+   
+   
+    return(
+        <div className={styles.currentframe} >
+            <h1
+              >Currently Playing</h1>
+            <div className={styles.currentCard}>
+                <div className={styles.imgBox}>
+                    <img src={currentimg} alt="" />
+                </div>
+
+                <div className={styles.currentSongDetails}>
+                    <h1>
+                     {currentSong.item.name}
+                    </h1>
+
+                    <h2
+                     style={{
+                      
+                      marginTop: "10px",
+                     
+                    }}>
+                       {singer}
+                    </h2>
+                </div>
+
+            
+
+                
+
+            </div>
+        </div>
+    )
+}
+
+export default CurrentlyPlaying;
