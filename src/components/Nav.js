177594ea13c77353cd9dc7f026ddebaa2a8d82d3
Tess-Vanta <tejaswimhtp@gmail.com>
2022-03-09 21:27:56 +0400

    Linked logo to home page

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,48 +1,36 @@
-import styles from '../styles/nav.module.css';
-import {  FaRegUserCircle} from "react-icons/fa";
+import styles from "../styles/nav.module.css";
+import { FaRegUserCircle } from "react-icons/fa";
 import logo from "../Logos/SPOTLIGHT-DARK-1-transparent.png";
-import {
-   
-    Link
-  } from "react-router-dom";
+import { Link } from "react-router-dom";
 
-function Nav(){
-    return(
-        <div>
-      
-
+function Nav() {
+  return (
+    <div>
       <div className={styles.navBOX}>
-        
         <div className={styles.navImg}>
-           
-          <img src={logo} alt="" className={styles.logo}  />
+          <a href="/">
+            <img src={logo} alt="" className={styles.logo} />
+          </a>
         </div>
 
-        <div className={styles.navDetail} >
+        <div className={styles.navDetail}>
           <div>
-          <Link to="/profile" className={styles.link}> < FaRegUserCircle className={styles.user} /></Link>
-        
+            <Link to="/profile" className={styles.link}>
+              {" "}
+              <FaRegUserCircle className={styles.user} />
+            </Link>
           </div>
 
-<div>
-<button>
-<Link className={styles.link} to="/login">
-Log Out
-</Link>
-</button>
-</div>
+          <div>
+            <button>
+              <Link className={styles.link} to="/login">
+                Log Out
+              </Link>
+            </button>
+          </div>
         </div>
-
-
       </div>
-
-
-
-
-
-
-        </div>
-    )
-
+    </div>
+  );
 }
 export default Nav;
